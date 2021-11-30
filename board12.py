1b468df15929889e6b948d4bc69f47b473d457ac
MadhanNadella <madhanmoni01@gmail.com>
2021-11-30 14:31:15 +0530

    Interfance and functionality, 1

--- board12.py
+++ board12.py
@@ -1,17 +1,24 @@
 import random
-import keyboard
 
+max=12
 sign = lambda a: ((a>0)-(a<0))
 transpose= lambda m: [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))]
 
+def power(list): 
+    return [2**number if number>0 else 0 for number in list]
+
 def arrange(matrix, num, lor):
     row=matrix[num]
+
     if(lor==1):
         resrow=[]
+        ismax=0
         for i in range(1,4):
             if(row[i]!=0 and row[i]==row[i-1]):
                 resrow.append(row[i]+1)
-                row[i]=0;
+                if(row[i]+1==max):
+                    ismax=1
+                row[i]=0
         matrix[num]=resrow
 
     elif(lor==-1):
@@ -20,11 +27,13 @@ def arrange(matrix, num, lor):
         for i in range(-2,1):
             if(row[i]!=0 and row[i]==row[i+1]):
                 resrow[elem]=row[i]+1
+                if(row[i]+1==max):
+                    ismax=1
                 elem-=1
                 row[i]=0;
         matrix[num]=resrow
     
-    return matrix
+    return matrix, ismax
 class GameBoard:
     def __init__(self):
         self.matrix=[]
@@ -39,7 +48,7 @@ class GameBoard:
 
         (self.matrix[r1][c1], self.matrix[r2][c2])= random.choices([1,2], weights=(9,1), k=2)
 
-    def regenrerate(self):
+    def regenerate(self):
         empties=[]
         for i in range(4):
             for j in range(4):
@@ -49,14 +58,22 @@ class GameBoard:
         self.matrix[p1][p2]=random.choices([1,2], weights=(9,1))
 
     def rearrange_rows(self, lor):
+        temp=self.matrix
         for i in range(3):
-            self.matrix= arrange(self.matrix,i, lor)
+            self.matrix, ismax = arrange(self.matrix,i, lor)
+        if(temp==self.matrix):
+            ismax=-1
+        return ismax
 
-    def rearrange_col(self, lor):
+    def rearrange_cols(self, lor):
+        temp=self.matrix
         mat=transpose(self.matrix)
         for i in range(3):
-            self.matrix=transpose(arrange(mat,i, lor))
+            self.matrix, ismax =transpose(arrange(mat,i, lor))
+        if(temp==self.matrix):
+            ismax=-1
+        return ismax
 
     def printmatrix(self):
-        for i in range(3):
-            print(self.matrix[i], "\n")
+        for i in range(4):
+            print(power(self.matrix[i]))
---
+++ main.py
@@ -0,0 +1,127 @@
+import board12 as bd
+# import keyboard as kb
+
+
+
+# print(
+#     "Instructions:
+#     Hi, welcome to 
+#     ")
+
+instructions = """
+Hi, welcome to 2048!
+Press Y to start the game
+You can press Q at any point in the game to exit the game
+"""
+
+print(instructions)
+
+while(1):
+    inp=input("Key: ")
+    # print(inp)
+    if(inp=='y' or inp=='Y'):
+        print("Great! Let's start the game")
+        break
+    elif(inp=='Q' or inp=='q'):
+        print("Exiting the game.")
+        exit()
+    else:
+        print("Invalid Key\n")
+
+
+# a.printmatrix()
+
+# instructions="""
+# Press A S D W keys to swipe left, down, right and up. 
+# """
+
+# while(1):
+#     inp=input("Key: ")
+#     print(inp)
+#     ismax=0
+#     if(inp=='A' or inp == 'a'):
+#         ismax= a.rearrange_rows(1)
+#         a.regenerate()
+#         a.printmatrix()
+#     elif(inp=='D' or inp == 'd'):
+#         ismax= a.rearrange_rows(-1)
+#         a.regenerate()
+#         a.printmatrix()
+#     elif(inp=='W' or inp == 'w'):
+#         ismax = a.rearrange_cols(1)
+#         a.regenerate()
+#         a.printmatrix()
+#     elif(inp=='S' or inp == 's'):
+#         ismax = a.rearrange_rows(-1)
+#         a.regenerate()
+#         a.printmatrix()
+#     elif(inp=='Q' or inp=='q'):
+#         print("Exiting the game.")
+#         exit()
+#     else:
+#         print("Invalid key")
+
+#     if(ismax==1):
+#         print("Congratulations! You won!!")
+#     elif(ismax==-1):
+#         print("You lost. Better luck next time")
+
+def play():
+    a=bd.GameBoard()
+    a.generate_initial()
+    a.printmatrix()
+
+    instructions="""
+Press A S D W keys to swipe left, down, right and up. 
+    """
+    print(instructions)
+
+    while(1):
+        inp=input("Key: ")
+        # print(inp)
+        ismax=0
+        if(inp=='A' or inp == 'a'):
+            ismax= a.rearrange_rows(1)
+            a.regenerate()
+            a.printmatrix()
+        elif(inp=='D' or inp == 'd'):
+            ismax= a.rearrange_rows(-1)
+            a.regenerate()
+            a.printmatrix()
+        elif(inp=='W' or inp == 'w'):
+            ismax = a.rearrange_cols(1)
+            a.regenerate()
+            a.printmatrix()
+        elif(inp=='S' or inp == 's'):
+            ismax = a.rearrange_rows(-1)
+            a.regenerate()
+            a.printmatrix()
+        elif(inp=='Q' or inp=='q'):
+            print("Exiting the game.")
+            exit()
+        else:
+            print("Invalid key")
+
+        if(ismax==1):
+            print("Congratulations! You won!!")
+        elif(ismax==-1):
+            print("You lost. Better luck next time")
+
+while(1):
+    play()
+
+    replay="""
+Press R to play again. Press Q to quit.
+    """
+
+    while(1):
+        inp=input("Key: ")
+        # print(inp)
+        if(inp=='r' or inp=='R'):
+            print("Great! Let's start the game")
+            continue
+        elif(inp=='Q' or inp=='q'):
+            print("Exiting the game.")
+            exit()
+        else:
+            print("Invalid Key\n")
